1975d80d32b054b10bdcb9f98b3cf1dc4dbc106f
ItsArance <arance.kurmiak@gmail.com>
2021-10-25 11:04:20 +0530

    Changes in models,buyed_products,views.py

--- myproject/user_app/models.py
+++ myproject/user_app/models.py
@@ -27,13 +27,13 @@ class Product(models.Model):
 
 class User(models.Model):
 	userName = models.CharField(max_length=40)
-	userEmail = models.EmailField(max_length=60)
+	userEmail = models.EmailField(max_length=60,unique=True)
 	userPassword = models.CharField(max_length=20)
 	userContact = models.CharField(max_length=20)
 	userAddedDate = models.DateTimeField(auto_now_add=True)
 	userProducts = models.ManyToManyField(Product,related_name="userProducts")
 	userCart = models.ManyToManyField(Product,related_name="userCart")
-	userBuyedProducts=models.ManyToManyField(Product,related_name="userBuyedProducts")
+	#userBuyedProducts=models.ManyToManyField(Product,related_name="userBuyedProducts")
 	userAddress = models.CharField(max_length=200)
 	def __str__(self):
 		return f'{self.userName}'
@@ -48,3 +48,19 @@ class Review(models.Model):
 	
 	def __str__(self):
 		return f'{self.reviewTitle} - {self.reviewTitle}'
+
+
+STATUS_CHOICES = [
+    ("Pending", "Pending"),
+    ("Delivered", "Delivered"),
+]
+
+class Sales(models.Model):
+	saleUser=models.ForeignKey(User,on_delete=models.CASCADE)
+	saleProduct=models.ForeignKey(Product,on_delete=models.CASCADE)
+	saleAddedDate = models.DateTimeField(auto_now_add=True)
+	saleAddress = models.CharField(max_length=200)
+	status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+	salePrice = models.IntegerField()
+	def __str__(self):
+		return f'{self.saleUser} - {self.saleProduct}'
